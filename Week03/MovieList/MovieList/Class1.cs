269756786c5894ade7577203b515bdc71ac5b196
Ashwin-Chirumamilla <ashwinchirumamilla@gmail.com>
2019-10-09 19:27:06 -0400

    Started

---
+++ Week03/MovieList/MovieList/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MovieList
+{
+    public class Movie 
+    {
+        public string Title;
+        public int releaseYear;
+
+        public Movie()
+        {
+
+        }
+
+        public void ShowDetails()
+        {
+            MessageBox.Show(Title + " was released in " + releaseYear);
+        }
+    }
+}
