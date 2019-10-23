eef72d737aab2070425a5dc69199fb038fa5f88d
Ashwin-Chirumamilla <ashwinchirumamilla@gmail.com>
2019-10-23 19:30:05 -0400

    Week 5

--- Week03/MovieList/MovieList/MainWindow.xaml.cs
+++ Week03/MovieList/MovieList/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace MovieList
     public partial class MainWindow : Window
     {
         List<Movie> movies = new List<Movie>();
+        int movieCycle = 0;
 
         public MainWindow()
         {
@@ -31,9 +32,36 @@ namespace MovieList
 
         private void addMovie_Click(object sender, RoutedEventArgs e)
         {
-            movies.Add(new Movie(movieTitle.Text, Convert.ToInt32(releaseYear.Text), directorName.Text, Convert.ToInt32(movieLength.Text), movieGenre.Text, posterFile.Text));
+            try
+            {
+                movies.Add(new Movie(movieTitle.Text, releaseYear.SelectedDate, directorName.Text, Convert.ToInt32(movieLength.Text), movieGenre.Text, posterFile.Text, Convert.ToDouble(rottenTomatoes.Text)));
+                MessageBox.Show("The movie was successfully added to your list.");
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show("There was an error in your Movie Data.");
+     
+            }
+            for(int i =0; i < movies.Count - 1; i++)
+            {
+                Movie listMovie = movies[i];
+                Movie currentMovie = movies[movies.Count - 1];
+                if (currentMovie.Title == listMovie.Title)
+                {
+                    movies.RemoveAt(movies.Count - 1);
+                    MessageBox.Show("You already added a movie with that title");
+                    break;
+                }
+            }
             movieTitle.Text = "";
             releaseYear.Text = "";
+            directorName.Text = "";
+            movieLength.Text = "";
+            movieGenre.Text = "";
+            posterFile.Text = "";
+            rottenTomatoes.Text = "";
+
+            
         }
 
         private void showMovies_Click(object sender, RoutedEventArgs e)
@@ -48,14 +76,21 @@ namespace MovieList
 
         private void rotatePosters_Click(object sender, RoutedEventArgs e)
         {
-            
-            BitmapImage poster = new BitmapImage();
-            poster.BeginInit();
-            poster.UriSource = new Uri(posterFile.Text);
-            poster.EndInit();
-            moviePoster.Source = poster;
-
-
+                
+               Movie currentMovie = movies[movieCycle];
+                BitmapImage poster = new BitmapImage();
+                poster.BeginInit();
+                poster.UriSource = new Uri(currentMovie.poster);
+                poster.EndInit();
+                moviePoster.Source = poster;
+            if (movieCycle < movies.Count - 1)
+            {
+                movieCycle++;
+            }
+            else
+            {
+                movieCycle = 0;
+            }
         }
     }
 }
--- Week03/MovieList/MovieList/MovieMain.cs
+++ Week03/MovieList/MovieList/MovieMain.cs
@@ -5,28 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
+
 namespace MovieList
 {
     public class Movie 
     {
         public string Title;
-        public int releaseYear;
+        public DateTime releaseYear { get; set; }
         public string director;
         public int length;
         public string genre;
         public string poster;
-        public Movie(string title, int release, string director, int length, string genre, string poster)
+        public double rottenTomatoesScore;
+        public Movie(string title, DateTime release, string director, int length, string genre, string poster, double rottenScore)
         {
             Title = title;
             releaseYear = release;
             this.director = director;
+            this.poster = poster;
             this.length = length;
             this.genre = genre;
+            this.rottenTomatoesScore = rottenScore;
+
+            
         }
 
         public void ShowDetails()
         {
-            MessageBox.Show(Title + " was released in " + releaseYear + ", directed by " + director + ", is " + length + " hours long, and is a " + genre);
+            MessageBox.Show(Title + " was released in " + releaseYear + ", directed by " + director + ", is " + length + " hours long, is a " + genre + ", and has a rotten tomatoes of " + rottenTomatoesScore);
         }
     }
     
